d3a08561cd8d3327e7e865c7551f734d5bb4c179
belokon <belokon.pavlik99@mail.ru>
2023-02-23 20:41:37 +0300

    homework2,task3

--- homework2/task2.py
+++ homework2/task2.py
@@ -1,6 +1,18 @@
-import random
-numbers = []
-random.shuffle(numbers)
-print(numbers)  
-random_number = random.choice(numbers)
-print(random_number)
+""" Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике.
+Он задумывает два натуральных числа X и Y(X, Y≤1000), а Катя должна их отгадать. Для этого Петя делает две подсказки. 
+ Он называет сумму этих чисел S и их произведение P. 
+Помогите Кате отгадать задуманные Петей числа. """
+while True:
+
+    sum = int(input("Ваша сумма чисел: "))
+    multiplication = int(input("Ваше произведение чисел: "))
+
+    if sum <= 2000 and multiplication <= 1000000:
+
+        break
+
+    print("Введены неверные данные")
+for x in range(0, 1001):
+    for y in range(0, 1001):
+        if x + y == sum and x*y == multiplication:
+            print(f"x = {x} y = {y}")
